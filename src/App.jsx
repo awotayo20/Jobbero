2cc7f6bd1dd0be856a2762b6221a7ac5c62a407f
awotayo20 <50973905+awotayo20@users.noreply.github.com>
2024-02-20 17:53:39 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,12 @@ import JobSearch from './Pages/Job Pages/JobSearch'
 import Application from './Pages/Job Pages/Application'
 import ApplicationSuccess from './Pages/Job Pages/ApplicationSuccess'
 
+/**
+ * This function handles private routes in a React application.
+ * It checks if the user is authenticated and returns the appropriate component based on the user's authentication status.
+ *
+ * @returns {JSX.Element} The component to render based on the user's authentication status.
+ */
 function PrivateRoutes() {
   const { user } = useAuth()
   return user ? <Outlet /> : <Navigate to="/login" />
